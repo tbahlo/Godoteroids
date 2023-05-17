b4bc157de1df6570ed4509339803eda437c77fe4
Thore Bahlo <ThO_orF@web.de>
2023-05-17 16:59:40 +0200

    Fragmenterzeugung und Ladungsinteraktion implementiert.

--- scenes/chargeroid.gd
+++ scenes/chargeroid.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 @export var chargeroid_tier = 0
-@export var charge = 100.0
+@export var charge = 50.0
+var amount_of_fragments = 2
 
 var chargeroidTemplate = preload("res://scenes/chargeroid.tscn")
 var velocity_vector = Vector2(0, 0)
@@ -19,8 +20,14 @@ func _ready():
 func _process(delta):
 	position = position + velocity_vector * delta
 	rotation += deg_to_rad(turn_rate * delta)
+	
+	if not is_alive:
+		$ChargeroidBody/ChargeroidSprite.modulate = Color(1, 0, 0)
 
-func _on_chargeroid_body_area_entered(area):
-	if "rojectile" in area.to_string():
+func _on_chargeroid_body_area_entered(body):
+	if "rojectile" in body.to_string():
+		var projectile = body.get_parent()
+		velocity_vector += 0.05 * projectile.velocity_vector
 		hit.emit()
 		is_alive = false
+		projectile.queue_free()
--- scenes/main.gd
+++ scenes/main.gd
@@ -4,6 +4,7 @@ var fps_value : int
 var uptime = 0
 var projectileTemplate = preload("res://scenes/projectile.tscn")
 var chargeroidTemplate = preload("res://scenes/chargeroid.tscn")
+var charged_actors = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,6 +14,7 @@ func _ready():
 		print("Added object: %s" % chargeroid.to_string())
 		chargeroid.position = Vector2(randf_range(0, 1600), randf_range(0, 800))
 		#chargeroid.velocity_vector = Vector2(randf_range(-25, 25), randf_range(-25, 25))
+		charged_actors.append(chargeroid)
 		add_child(chargeroid)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,18 +22,41 @@ func _process(delta):
 	fps_value = 1/delta
 	uptime += delta
 	
-	for child in get_children():
-		if "argeroid" in child.to_string():
-			if child.is_alive == false:
-				if child.chargeroid_tier <= 2:
+	#apply_charge_interaction()
+	spawn_fragments()
+
+func apply_charge_interaction():
+	for i in range(len(charged_actors)):
+		var first_body = charged_actors[i]
+		for j in range(i+1, len(charged_actors)):
+			var second_body = charged_actors[j]
+			var distance_vector = first_body.position - second_body.position
+			var distance = distance_vector.length()
+			var velocity_change = first_body.charge * second_body.charge
+			velocity_change /= distance**2
+			first_body.velocity_vector += velocity_change * distance_vector.abs()
+			second_body.velocity_vector -= velocity_change * distance_vector.abs()
+
+func spawn_fragments():
+	for actor in charged_actors:
+		if actor.is_alive == false:
+			if actor.chargeroid_tier <= 2:
+				print(range(actor.amount_of_fragments))
+				for i in range(actor.amount_of_fragments):
 					var fragment = chargeroidTemplate.instantiate()
-					fragment.position = child.position
-					fragment.velocity_vector = child.velocity_vector
-					fragment.chargeroid_tier = child.chargeroid_tier + 1
-					var scale_factor = 1./(2**(fragment.chargeroid_tier))+0.25
+					fragment.position = actor.position
+					#fragment.position += Vector2(randf_range(-75, 75), randf_range(-75, 75))
+					fragment.velocity_vector = actor.velocity_vector
+					fragment.velocity_vector += Vector2(randf_range(-75, 75), randf_range(-75, 75))
+					
+					fragment.chargeroid_tier = actor.chargeroid_tier + 1
+					
+					var scale_factor = 1./(2**(fragment.chargeroid_tier))+0.5
 					fragment.apply_scale(Vector2(scale_factor, scale_factor))
 					add_child(fragment)
-				child.queue_free()
+					charged_actors.append(fragment)
+			charged_actors.erase(actor)
+			actor.queue_free()
 
 func _on_debug_msg_timer_timeout():
 	var prefix = "[%.3f] " % uptime
